e162a01f24493c242255ae51d04418d52a8ac9b2
James Trimble <james.trimble@yahoo.co.uk>
2014-06-27 16:18:22 +0100

    Added missing import for slugify

--- dnd_project/dragndrop/models.py
+++ dnd_project/dragndrop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from urlparse import urlparse
