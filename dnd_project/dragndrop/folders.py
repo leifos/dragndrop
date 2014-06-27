dc743773a9579c30b48c3536eaca2638892bb801
Leif Azzopardi <leifos@acm.org>
2014-06-27 16:34:34 +0100

    included suggest_folders to tests.py and folders.py

--- dnd_project/dragndrop/folders.py
+++ dnd_project/dragndrop/folders.py
@@ -1,7 +1,10 @@
 __author__ = 'leif'
 
 from models import Folder, Bookmark
+import operator
 from django.contrib.auth.models import User
+import re
+from django.db.models import Q
 
 def create_folder(user, folder_name):
     """
@@ -23,10 +26,80 @@ def remove_folder(folder_id):
     f = get_folder(folder_id)
     if f:
         f.delete()
+"""
+methods
+    normalize_query(), get_query()
 
+    adapted from:
+    http://julienphalip.com/post/2825034077/adding-search-to-a-django-site-in-a-snap
 
-def suggest_folder(user, query):
-    pass
+"""
+
+def __normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
+        and grouping quoted words together.
+        Example:
+
+        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+
+    '''
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+def __get_query(query_string, search_fields):
+    ''' Returns a query, that is a combination of Q objects. That combination
+        aims to search keywords within a model by testing the given search fields.
+
+    '''
+    query = None # Query to search for every search term
+    terms = __normalize_query(query_string)
+    for term in terms:
+        or_query = None # Query to search for a given term in each field
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
+
+def suggest_folders(user, query):
+    """
+    this function searches the bookmarks model's title and description for the given
+    query_string
+    it returns the top three folders in which the bookmarks contain the query_string
+
+    functions used: get_query
+    """
+
+    found_entries = None
+
+    entry_query = __get_query(query, ['title', 'summary',])
+
+    found_entries = Bookmark.objects.filter(entry_query).order_by('-clicks')
+
+    context_dict = {}
+
+    for result in found_entries:
+        folder_res = result.folder
+        if folder_res.user == user:
+            if folder_res in context_dict:
+                context_dict[folder_res] += 1
+            else:
+                context_dict[folder_res] = 1
+            folder_res.url = folder_res.name.replace(' ', '_')
+
+    sorted_dict = sorted(context_dict.iteritems(), key=operator.itemgetter(1))[::-1]
+
+    top_three = [i[0] for i in sorted_dict][:3]
+
+    return top_three
 
 def get_folder(folder_id):
     """
--- dnd_project/dragndrop/tests.py
+++ dnd_project/dragndrop/tests.py
@@ -10,7 +10,8 @@ from models import Folder
 from find.search.response import Result
 from django.contrib.auth.models import User
 from models import Bookmark
-from folders import create_folder, get_user_folders, remove_folder, get_folder, add_result_to_folder, get_folder_results, get_user_folders
+import re
+from folders import create_folder, get_user_folders, remove_folder, get_folder, add_result_to_folder, get_folder_results, get_user_folders, suggest_folders
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -81,3 +82,26 @@ class SimpleTest(TestCase):
         f3 = get_user_folders(self.ie)
 
         self.assertItemsEqual([f1,f2],f3)
+
+    def test_suggestion_of_folders(self):
+        f1 = create_folder(self.ie,'test this')
+        f2 = create_folder(self.ie,'test that')
+        f3 = create_folder(self.ie,'test everything')
+
+        r1 = Result(title='r1',url='www.love.com',summary="Gambo Love")
+        r2 = Result(title='r2',url='www.isinthe.com',summary="Gambo Food")
+        r3 = Result(title='r3',url='www.air.com',summary="Gambo Money")
+        r4 = Result(title='r4',url='www.james.com',summary="Gambo Food")
+        r5 = Result(title='r5',url='www.ryanair.com',summary="Gambo Money")
+
+        b1 = add_result_to_folder(f1.id, r1)
+        b2 = add_result_to_folder(f1.id, r2)
+        b3 = add_result_to_folder(f1.id, r3)
+        b4 = add_result_to_folder(f2.id, r4)
+        b5 = add_result_to_folder(f3.id, r5)
+
+        f5 = suggest_folders(self.ie,'money')
+
+        self.assertItemsEqual([f1,f3], f5)
+
+
