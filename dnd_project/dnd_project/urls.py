0c0150ca4bfa904692b35fee1cef15b04c944104
David Maxwell <maxwelld90@gmail.com>
2014-06-27 16:39:52 +0100

    Added basic user auth stuff to base.html

--- dnd_project/dnd_project/urls.py
+++ dnd_project/dnd_project/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/register/$', RegistrationView.as_view()),
-    url(r'^accounts/$', include('registration.backends.simple.urls')),  # One-step django-registration backend.
+    url(r'^accounts/', include('registration.backends.simple.urls')),  # One-step django-registration backend.
     url(r'^', include('dragndrop.urls')),
 )
 
