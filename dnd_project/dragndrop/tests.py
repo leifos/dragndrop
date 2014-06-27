846cd8f7baadfb3378860441be1ce8e670401392
Leif Azzopardi <leifos@acm.org>
2014-06-27 15:28:41 +0100

    updated tests.py and folders.py

--- dnd_project/dragndrop/tests.py
+++ dnd_project/dragndrop/tests.py
@@ -9,7 +9,8 @@ from django.test import TestCase
 from models import Folder
 from find.search.response import Result
 from django.contrib.auth.models import User
-from folders import create_folder, get_user_folders, remove_folder, get_folder
+from models import Bookmark
+from folders import create_folder, get_user_folders, remove_folder, get_folder, add_result_to_folder, get_folder_results, get_user_folders
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -47,8 +48,36 @@ class SimpleTest(TestCase):
         folder_list = get_user_folders(self.ie)
         self.assertEquals(len(folder_list),1)
 
+    def test_get_results_of_folder(self):
+        f1 = create_folder(self.ie,'test')
+
+        r1 = Result(title='r1',url='www.love.com',summary="Gambo Gambo")
+        r2 = Result(title='r2',url='www.isinthe.com',summary="Gambo Gambo")
+        r3 = Result(title='r3',url='www.air.com',summary="Gambo Gambo")
+
+        b1 = add_result_to_folder(f1.id, r1)
+        b2 = add_result_to_folder(f1.id, r2)
+        b3 = add_result_to_folder(f1.id, r3)
+
+        b4 = get_folder_results(f1.id)
+
+        self.assertItemsEqual([b1, b2, b3], b4)
+
     def test_addition_of_results_to_folder(self):
         f1 = create_folder(self.ie,'test')
 
+        r1 = Result(title='r1',url='www.love.com',summary="Gambo Gambo")
 
+        b1 = Bookmark(folder=f1,title=r1.title, url=r1.url, summary=r1.summary)
+        b1.save()
 
+        b2 = add_result_to_folder(f1.id, r1)
+        self.assertEquals(b1,b2)
+
+    def test_get_user_folders(self):
+        f1 = create_folder(self.ie,'test')
+        f2 = create_folder(self.ie,'temp')
+
+        f3 = get_user_folders(self.ie)
+
+        self.assertItemsEqual([f1,f2],f3)
